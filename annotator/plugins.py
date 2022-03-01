90fb15d922d44f7537c47c36c1967fa6030193b8
achard <cyril.achard@epfl.ch>
2022-03-01 11:22:55 +0100

    file struct+setup/cfg files

--- annotator/plugins.py
+++
@@ -1,9 +0,0 @@
-from annotator import Loader, Trainer, Predicter
-
-
-def napari_experimental_provide_dock_widget():
-    return [
-        (Loader, {"name": "File loader"}),
-        (Trainer, {"name": "Train"}),
-        (Predicter, {"name": "Predict"}),
-    ]
