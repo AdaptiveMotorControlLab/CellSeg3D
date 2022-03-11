6a0c38ca0a7ed3d8718774767d43f089dbc434d0
C-Achard <cyril.achard@epfl.ch>
2022-03-11 11:01:00 +0100

    removed unused scripts

--- napari_cellseg_annotator/view_brain.py
+++
@@ -1,9 +0,0 @@
-import napari
-
-from dask_image.imread import imread
-
-y = imread("/Users/maximevidal/Documents/3drawdata/wholebrain.tif")
-
-with napari.gui_qt():
-    viewer = napari.Viewer()
-    viewer.add_image(y, contrast_limits=[0, 2000], multiscale=False)
