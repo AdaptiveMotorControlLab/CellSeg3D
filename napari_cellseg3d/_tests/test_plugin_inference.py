7a8f6d4bf220c9abe29ff0239c7ccf82868b4464
C-Achard <cyril.achard@epfl.ch>
2023-03-02 10:20:29 +0100

    Testing updates

--- napari_cellseg3d/_tests/test_plugin_inference.py
+++ napari_cellseg3d/_tests/test_plugin_inference.py
@@ -1,21 +1,8 @@
 from tifffile import imread
 from pathlib import Path
-import warnings
 
-from qtpy.QtWidgets import QTextEdit
-
+from napari_cellseg3d._tests.fixtures import LogFixture
 from napari_cellseg3d.code_plugins.plugin_model_inference import Inferer
-
-
-class LogFixture(QTextEdit):
-    def __init__(self):
-        super(LogFixture, self).__init__()
-
-    def print_and_log(self, text, printing=None):
-        print(text)
-
-    def warn(self, warning):
-        warnings.warn(warning)
 
 
 def test_inference(make_napari_viewer, qtbot):
@@ -39,10 +26,10 @@ def test_inference(make_napari_viewer, qtbot):
 
     assert widget.check_ready()
 
-    widget.start()
-    assert widget.worker is not None
-
-    with qtbot.waitSignal(signal=widget.worker.finished) as blocker:
-        blocker.connect(widget.worker.errored)
-
-    assert len(viewer.layers) == 2
+    # widget.start()  # takes too long on Github Actions (~30min)
+    # assert widget.worker is not None
+    #
+    # with qtbot.waitSignal(signal=widget.worker.finished) as blocker:
+    #     blocker.connect(widget.worker.errored)
+    #
+    # assert len(viewer.layers) == 2
--- napari_cellseg3d/_tests/test_training.py
+++ napari_cellseg3d/_tests/test_training.py
@@ -1,27 +1,44 @@
+from pathlib import Path
+
 from napari_cellseg3d import config
-from napari_cellseg3d.code_plugins import plugin_model_training as train
+from napari_cellseg3d.code_plugins.plugin_model_training import Trainer
+from napari_cellseg3d._tests.fixtures import LogFixture
 
 
-def test_check_ready(make_napari_viewer):
-    view = make_napari_viewer()
-    widget = train.Trainer(view)
+def test_training(make_napari_viewer, qtbot):
+    im_path = str(Path(__file__).resolve().parent / "res/test.tif")
+
+    viewer = make_napari_viewer()
+    widget = Trainer(viewer)
+    widget.log = LogFixture()
+    viewer.window.add_dock_widget(widget)
 
     widget.images_filepath = None
     widget.labels_filepaths = None
 
-    res = widget.check_ready()
-    assert not res
+    assert not widget.check_ready()
 
-    # widget.images_filepath = ["C:/test/something.tif"]
-    # widget.labels_filepaths = ["C:/test/lab_something.tif"]
-    # res = widget.check_ready()
+    assert widget.filetype_choice.currentText() == ".tif"
+
+    widget.images_filepaths = [im_path]
+    widget.labels_filepaths = [im_path]
+
+    assert widget.check_ready()
+
+    #################
+    # Training is too long to test properly this way. Do not use on Github
+    #################
+
+    # widget.start()
+    # assert widget.worker is not None
     #
-    # assert res
+    # with qtbot.waitSignal(signal=widget.worker.finished, timeout=60000) as blocker:  # wait only for 60 seconds.
+    #     blocker.connect(widget.worker.errored)
 
 
 def test_update_loss_plot(make_napari_viewer):
     view = make_napari_viewer()
-    widget = train.Trainer(view)
+    widget = Trainer(view)
 
     widget.worker_config = config.TrainingWorkerConfig()
     widget.worker_config.validation_interval = 1
