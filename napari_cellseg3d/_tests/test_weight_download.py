e2295a16b446c80f3ee2579fe3f195db1965c5e1
C-Achard <cyril.achard@epfl.ch>
2023-03-03 14:44:31 +0100

    Updated actions version

--- napari_cellseg3d/_tests/test_weight_download.py
+++ napari_cellseg3d/_tests/test_weight_download.py
@@ -5,9 +5,8 @@ from napari_cellseg3d.code_models.model_workers import (
 
 # DISABLED, causes GitHub actions to freeze
 def test_weight_download():
-    pass
-    # downloader = WeightsDownloader()
-    # downloader.download_weights("test", "test.pth")
-    # result_path = WEIGHTS_DIR / "test.pth"
-    #
-    # assert result_path.is_file()
+    downloader = WeightsDownloader()
+    downloader.download_weights("test", "test.pth")
+    result_path = WEIGHTS_DIR / "test.pth"
+
+    assert result_path.is_file()
