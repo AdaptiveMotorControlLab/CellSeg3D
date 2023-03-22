b7f571f141ae9e23e1a9ef0c1840f94df93413b6
C-Achard <cyril.achard@epfl.ch>
2023-03-22 15:08:05 +0100

    black

--- napari_cellseg3d/config.py
+++ napari_cellseg3d/config.py
@@ -115,10 +115,12 @@ class Zoom:
     enabled: bool = True
     zoom_values: List[float] = None
 
+
 @dataclass
 class InstanceSegConfig:
     enabled: bool = False
     method: InstanceMethod = None
+
 
 @dataclass
 class InstanceSegConfig:
