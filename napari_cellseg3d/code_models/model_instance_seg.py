Fixed wrong value in instance sliders

napari_cellseg3d/code_models/model_instance_seg.py
@@ -138,6 +138,9 @@ def voronoi_otsu(
     """
     # remove_small_size (float): remove all objects smaller than the specified size in pixels
     semantic = np.squeeze(volume)
+    logger.debug(
+        f"Running voronoi otsu segmentation with spot_sigma={spot_sigma} and outline_sigma={outline_sigma}"
+    )
     instance = cle.voronoi_otsu_labeling(
         semantic, spot_sigma=spot_sigma, outline_sigma=outline_sigma
     )
@@ -146,7 +149,7 @@ def voronoi_otsu(
 
 
 def binary_connected(
-    volume,
+    volume: np.array,
     thres=0.5,
     thres_small=3,
 ):
@@ -158,8 +161,12 @@ def binary_connected(
         thres (float): threshold of foreground. Default: 0.8
         thres_small (int): size threshold of small objects to remove. Default: 128
     """
+    logger.debug(
+        f"Running connected components segmentation with thres={thres} and thres_small={thres_small}"
+    )
+    # if len(volume.shape) > 3:
     semantic = np.squeeze(volume)
-    foreground = semantic > thres  # int(255 * thres)
+    foreground = np.where(semantic > thres, volume, 0)  # int(255 * thres)
     segm = label(foreground)
     segm = remove_small_objects(segm, thres_small)
 
@@ -202,6 +209,10 @@ def binary_watershed(
         rem_seed_thres (int): threshold for small seeds removal. Default : 3
 
     """
+    logger.debug(
+        f"Running watershed segmentation with thres_objects={thres_objects}, thres_seeding={thres_seeding},"
+        f" thres_small={thres_small} and rem_seed_thres={rem_seed_thres}"
+    )
     semantic = np.squeeze(volume)
     seed_map = semantic > thres_seeding
     foreground = semantic > thres_objects
@@ -407,8 +418,8 @@ class Watershed(InstanceMethod):
     def run_method(self, image):
         return self.function(
             image,
-            self.sliders[0].value(),
-            self.sliders[1].value(),
+            self.sliders[0].slider_value,
+            self.sliders[1].slider_value,
             self.counters[0].value(),
             self.counters[1].value(),
         )
@@ -441,7 +452,7 @@ class ConnectedComponents(InstanceMethod):
 
     def run_method(self, image):
         return self.function(
-            image, self.sliders[0].value(), self.counters[0].value()
+            image, self.sliders[0].slider_value, self.counters[0].value()
         )
 
 
@@ -501,7 +512,7 @@ class InstanceWidgets(QWidget):
         """
         super().__init__(parent)
         self.method_choice = ui.DropdownMenu(
-            INSTANCE_SEGMENTATION_METHOD_LIST.keys()
+            list(INSTANCE_SEGMENTATION_METHOD_LIST.keys())
         )
         self.methods = {}
         """Contains the instance of the method, with its name as key"""
@@ -520,7 +531,7 @@ class InstanceWidgets(QWidget):
                 method_class = method(widget_parent=self.parent())
                 self.methods[name] = method_class
                 self.instance_widgets[name] = []
-                # moderately unsafe way to init those widgets
+                # moderately unsafe way to init those widgets ?
                 if len(method_class.sliders) > 0:
                     for slider in method_class.sliders:
                         group.layout.addWidget(slider.container)
@@ -530,8 +541,10 @@ class InstanceWidgets(QWidget):
                         group.layout.addWidget(counter.label)
                         group.layout.addWidget(counter)
                         self.instance_widgets[name].append(counter)
-        except RuntimeError:
-            logger.debug("Caught runtime error, most likely during testing")
+        except RuntimeError as e:
+            logger.debug(
+                f"Caught runtime error {e}, most likely during testing"
+            )
 
         self.setLayout(group.layout)
         self._set_visibility()
@@ -555,9 +568,7 @@ class InstanceWidgets(QWidget):
         Returns: processed image from self._method
 
         """
-        method = INSTANCE_SEGMENTATION_METHOD_LIST[
-            self.method_choice.currentText()
-        ]()
+        method = self.methods[self.method_choice.currentText()]
         return method.run_method(volume)
 
 