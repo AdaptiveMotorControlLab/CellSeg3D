Fixed wrong value in instance sliders

napari_cellseg3d/code_models/model_instance_seg.py
@@ -138,6 +138,9 @@ def voronoi_otsu(
     """
     # remove_small_size (float): remove all objects smaller than the specified size in pixels
     semantic = np.squeeze(volume)
+    logger.debug(
+        f"Running voronoi otsu segmentation with spot_sigma={spot_sigma} and outline_sigma={outline_sigma}"
+    )
     instance = cle.voronoi_otsu_labeling(
         semantic, spot_sigma=spot_sigma, outline_sigma=outline_sigma
     )
@@ -415,8 +418,8 @@ class Watershed(InstanceMethod):
     def run_method(self, image):
         return self.function(
             image,
-            self.sliders[0].value(),
-            self.sliders[1].value(),
+            self.sliders[0].slider_value,
+            self.sliders[1].slider_value,
             self.counters[0].value(),
             self.counters[1].value(),
         )
@@ -449,7 +452,7 @@ class ConnectedComponents(InstanceMethod):
 
     def run_method(self, image):
         return self.function(
-            image, self.sliders[0].value(), self.counters[0].value()
+            image, self.sliders[0].slider_value, self.counters[0].value()
         )
 
 
@@ -509,7 +512,7 @@ class InstanceWidgets(QWidget):
         """
         super().__init__(parent)
         self.method_choice = ui.DropdownMenu(
-            INSTANCE_SEGMENTATION_METHOD_LIST.keys()
+            list(INSTANCE_SEGMENTATION_METHOD_LIST.keys())
         )
         self.methods = {}
         """Contains the instance of the method, with its name as key"""
@@ -528,7 +531,7 @@ class InstanceWidgets(QWidget):
                 method_class = method(widget_parent=self.parent())
                 self.methods[name] = method_class
                 self.instance_widgets[name] = []
-                # moderately unsafe way to init those widgets
+                # moderately unsafe way to init those widgets ?
                 if len(method_class.sliders) > 0:
                     for slider in method_class.sliders:
                         group.layout.addWidget(slider.container)
@@ -538,8 +541,10 @@ class InstanceWidgets(QWidget):
                         group.layout.addWidget(counter.label)
                         group.layout.addWidget(counter)
                         self.instance_widgets[name].append(counter)
-        except RuntimeError:
-            logger.debug("Caught runtime error, most likely during testing")
+        except RuntimeError as e:
+            logger.debug(
+                f"Caught runtime error {e}, most likely during testing"
+            )
 
         self.setLayout(group.layout)
         self._set_visibility()
@@ -563,9 +568,7 @@ class InstanceWidgets(QWidget):
         Returns: processed image from self._method
 
         """
-        method = INSTANCE_SEGMENTATION_METHOD_LIST[
-            self.method_choice.currentText()
-        ]()
+        method = self.methods[self.method_choice.currentText()]
         return method.run_method(volume)
 
 