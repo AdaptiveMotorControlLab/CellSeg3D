8c5bd105eb78b20cbb6305a21ba771737cedc906
C-Achard <cyril.achard@epfl.ch>
2023-04-23 10:28:30 +0200

    Update test_plugin_utils.py

--- napari_cellseg3d/_tests/test_plugin_utils.py
+++ napari_cellseg3d/_tests/test_plugin_utils.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+from tifffile import imread
 import numpy as np
-from numpy.random import PCG64, Generator
 
+from napari_cellseg3d.code_plugins.plugin_utilities import Utilities
 from napari_cellseg3d.code_plugins.plugin_utilities import (
     UTILITIES_WIDGETS,
     Utilities,
@@ -13,9 +15,10 @@ def test_utils_plugin(make_napari_viewer):
     view = make_napari_viewer()
     widget = Utilities(view)
 
-    image = rand_gen.random((10, 10, 10)).astype(np.uint8)
-    image_layer = view.add_image(image, name="image")
-    label_layer = view.add_labels(image.astype(np.uint8), name="labels")
+    im_path = str(Path(__file__).resolve().parent / "res/test.tif")
+    image = imread(im_path)
+    view.add_image(image)
+    view.add_labels(image.astype(np.uint8))
 
     view.window.add_dock_widget(widget)
     view.dims.ndisplay = 3
@@ -24,11 +27,4 @@ def test_utils_plugin(make_napari_viewer):
         assert isinstance(
             widget.utils_widgets[i], UTILITIES_WIDGETS[utils_name]
         )
-        if utils_name == "Convert to instance labels":
-            # to avoid issues with Voronoi-Otsu missing runtime
-            menu = widget.utils_widgets[i].instance_widgets.method_choice
-            menu.setCurrentIndex(menu.currentIndex() + 1)
-
-        assert len(image_layer.data.shape) == 3
-        assert len(label_layer.data.shape) == 3
         widget.utils_widgets[i]._start()
