7b79d5e5dc63b761fad72dddf93b721da112e6d0
C-Achard <cyril.achard@epfl.ch>
2023-04-23 11:41:23 +0200

    Updated utils test to avoid Voronoi-Otsu
    VO is missing CL runtime

--- napari_cellseg3d/_tests/test_plugin_utils.py
+++ napari_cellseg3d/_tests/test_plugin_utils.py
@@ -27,4 +27,9 @@ def test_utils_plugin(make_napari_viewer):
         assert isinstance(
             widget.utils_widgets[i], UTILITIES_WIDGETS[utils_name]
         )
+        if utils_name == "Convert to instance labels":
+            # to avoid issues with Voronoi-Otsu missing runtime
+            menu = widget.utils_widgets[i].instance_widgets.method_choice
+            menu.setCurrentIndex(menu.currentIndex() + 1)
+
         widget.utils_widgets[i]._start()
