8d30d5ca395b7052a46a91292b53ca33a2d3d07b
C-Achard <cyril.achard@epfl.ch>
2023-05-16 17:31:36 +0200

    Slight coverage increase

--- napari_cellseg3d/_tests/test_plugin_inference.py
+++ napari_cellseg3d/_tests/test_plugin_inference.py
@@ -39,23 +39,14 @@ def test_inference(make_napari_viewer, qtbot):
     assert widget.worker_config is not None
     assert widget.model_info is not None
     worker = widget._create_worker_from_config(widget.worker_config)
+
     assert worker.config is not None
     assert worker.config.model_info is not None
     worker.config.layer = viewer.layers[0].data
+    worker.config.post_process_config.instance.enabled = True
     assert worker.config.layer is not None
     worker.log_parameters()
 
     res = next(worker.inference())
     assert isinstance(res, InferenceResult)
     assert res.result.shape == (6, 6, 6)
-
-    # def on_error(e):
-    #     print(e)
-    #     assert False
-    # with qtbot.waitSignal(
-    #     signal=worker.finished, timeout=10000, raising=True
-    # ) as blocker:
-    #     worker.error_signal.connect(on_error)
-    #     blocker.connect(worker.errored)
-    #     worker.inference()  # takes too long on Github Actions
-    # assert len(viewer.layers) == 2
