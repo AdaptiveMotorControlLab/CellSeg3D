3fafbb4c275df1cac5a89ca892e6c50364a8a548
C-Achard <cyril.achard@epfl.ch>
2023-07-12 17:57:55 +0200

    Added newest weight to HF for new WNet

--- napari_cellseg3d/_tests/test_models.py
+++ napari_cellseg3d/_tests/test_models.py
@@ -118,10 +118,6 @@ def test_pretrained_weights_compatibility():
         WeightsDownloader().download_weights(model_name, file_name)
         model = MODEL_LIST[model_name](input_img_size=[128,128,128])
         try:
-            if model_name == "WNet":
-                strict = False
-            else:
-                strict = True
-            model.load_state_dict(torch.load(str(Path(PRETRAINED_WEIGHTS_DIR) / file_name), map_location="cpu"), strict=strict)
+            model.load_state_dict(torch.load(str(Path(PRETRAINED_WEIGHTS_DIR) / file_name), map_location="cpu"), strict=True)
         except RuntimeError:
             pytest.fail(f"Failed to load weights for {model_name}")
