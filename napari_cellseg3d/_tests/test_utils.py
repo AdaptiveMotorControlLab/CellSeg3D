Normalization changes+ fix issues with default path

napari_cellseg3d/_tests/test_utils.py
@@ -209,16 +209,31 @@ def test_parse_default_path():
     user_path = Path().home()
     assert utils.parse_default_path([None]) == str(user_path)
 
-    test_path = "C:/test/test"
+    test_path = "C:/test/test/test/test"
     path = [test_path, None, None]
-    assert utils.parse_default_path(path) == test_path
+    assert utils.parse_default_path(path, check_existence=False) == test_path
+
+    test_path = "C:/test/does/not/exist"
+    path = [test_path, None, None]
+    assert utils.parse_default_path(path, check_existence=True) == str(
+        Path.home()
+    )
 
     long_path = "D:/very/long/path/what/a/bore/ifonlytherewas/something/tohelpmenotsearchit/allthetime"
     path = [test_path, None, None, long_path, ""]
-    assert utils.parse_default_path(path) == long_path
+    assert utils.parse_default_path(path, check_existence=False) == long_path
 
 
 def test_thread_test(make_napari_viewer_proxy):
     viewer = make_napari_viewer_proxy()
     w = thread_test.create_connected_widget(viewer)
     viewer.window.add_dock_widget(w)
+
+
+def test_quantile_norm():
+    array = rand_gen.random(size=(100, 100, 100))
+    low_quantile = np.quantile(array, 0.01)
+    high_quantile = np.quantile(array, 0.99)
+    array_norm = utils.quantile_normalization(array)
+    assert array_norm.min() >= low_quantile
+    assert array_norm.max() <= high_quantile

napari_cellseg3d/code_models/instance_segmentation.py
@@ -251,10 +251,10 @@ def binary_watershed(
         rem_seed_thres (int): threshold for small seeds removal. Default : 3
 
     """
-    logger.debug(
-        f"Running watershed segmentation with thres_objects={thres_objects}, thres_seeding={thres_seeding},"
-        f" thres_small={thres_small} and rem_seed_thres={rem_seed_thres}"
-    )
+    # logger.debug(
+    #     f"Running watershed segmentation with thres_objects={thres_objects}, thres_seeding={thres_seeding},"
+    #     f" thres_small={thres_small} and rem_seed_thres={rem_seed_thres}"
+    # )
     semantic = np.squeeze(volume)
     seed_map = semantic > thres_seeding
     foreground = semantic > thres_objects

napari_cellseg3d/utils.py
@@ -2,15 +2,13 @@ import logging
 import math
 from datetime import datetime
 from pathlib import Path
-from typing import TYPE_CHECKING, Union
+from typing import Union
 
 import napari
 import numpy as np
+import torch
 from monai.transforms import Zoom
 from tifffile import imread, imwrite
-
-if TYPE_CHECKING:
-    import torch
 
 LOGGER = logging.getLogger(__name__)
 ###############
@@ -394,7 +392,7 @@ def fill_list_in_between(lst, n, fill_value):
     return None
 
 
-def parse_default_path(possible_paths):
+def parse_default_path(possible_paths, check_existence=True):
     """Returns a default path based on a vector of paths, some of which might be empty.
 
     Args:
@@ -404,14 +402,18 @@ def parse_default_path(possible_paths):
 
     """
     default_paths = []
+    LOGGER.debug(f"Utils : possible paths are {possible_paths}")
     if any(path is not None for path in possible_paths):
-        default_paths = [
-            p for p in possible_paths if p is not None and len(p) > 2
-        ]
+        for p in possible_paths:
+            if p is not None and (Path(p).exists() or not check_existence):
+                default_paths.append(str(Path(p).resolve().as_posix()))
+        # default_paths = [
+        #     str(Path(p).resolve()) for p in possible_paths if (Path(p).exists())
+        # ]
     # default_paths = [
     #     path for path in default_paths if path is not None and path != []
     # ]
-    print(default_paths)
+    LOGGER.debug(f"Utils : default paths are {default_paths}")
     if len(default_paths) == 0:
         return str(Path().home())
     default_path = max(default_paths, key=len)
@@ -471,3 +473,32 @@ def load_images(dir_or_path, filetype="", as_folder: bool = False):
     #         "Loading as folder not implemented yet. Use napari to load as folder"
     # images_original = dask_imread(filename_pattern_original)
     #     )
+
+
+def quantile_normalization(
+    image: Union[np.ndarray, torch.Tensor],
+    quantile_high=0.99,
+    quantile_low=0.01,
+):
+    """Normalizes an image using the quantiles"""
+    if quantile_high < quantile_low:
+        raise ValueError(
+            f"quantile_high must be greater than quantile_low, got {quantile_high} and {quantile_low}"
+        )
+
+    if isinstance(image, torch.Tensor):
+        qtl = torch.quantile
+        where = torch.where
+    elif isinstance(image, np.ndarray):
+        qtl = np.quantile
+        where = np.where
+    else:
+        raise TypeError("image needs to be torch tensor or numpy array")
+
+    shape = image.shape
+    image = image.flatten()
+    high_quantile_value = qtl(image, quantile_high)
+    low_quantile_value = qtl(image, quantile_low)
+    image = where(image > high_quantile_value, high_quantile_value, image)
+    image = where(image < low_quantile_value, low_quantile_value, image)
+    return image.reshape(shape)