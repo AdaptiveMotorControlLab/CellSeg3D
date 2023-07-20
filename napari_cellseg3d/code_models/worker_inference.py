8958aec3a73dc7a4236c8e76847bc983474b8734
C-Achard <cyril.achard@epfl.ch>
2023-07-20 09:54:29 +0200

    Update worker_inference.py

--- napari_cellseg3d/code_models/worker_inference.py
+++ napari_cellseg3d/code_models/worker_inference.py
@@ -241,11 +241,10 @@ class InferenceWorker(GeneratorWorker):
         logger.debug(volume.shape)
         logger.debug(volume.dtype)
 
-        normalization = (
-            QuantileNormalization()
-            if self.config.model_info.name != "WNet"
-            else lambda x: x
-        )
+        normalization = QuantileNormalization()
+        # if self.config.model_info.name != "WNet"
+        # else lambda x: x
+        # )
         if self.config.sliding_window_config.is_enabled():
             load_transforms = Compose(
                 [
