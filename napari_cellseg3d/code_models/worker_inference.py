Fix rotation checks + auto-channel select

napari_cellseg3d/code_models/worker_inference.py
@@ -405,6 +405,23 @@ class InferenceWorker(GeneratorWorker):
             # sys.stdout = old_stdout
             # sys.stderr = old_stderr
 
+    def _correct_results_rotation(self, array, shape):
+        """Corrects the shape of the array if needed."""
+        if array is None:
+            return None
+        if array.shape[-3:] != shape[-3:]:
+            logger.debug(
+                f"Correcting rotation due to results shape mismatch: target {shape}, got {array.shape}"
+            )
+            array = utils.correct_rotation(array)
+            if (
+                array.shape[-3:] != shape[-3:]
+            ):  # check only non-channel dimensions
+                logger.warning(
+                    f"Results shape mismatch: target {shape}, got {array.shape}"
+                )
+        return array
+
     def create_inference_result(
         self,
         semantic_labels,
@@ -437,24 +454,12 @@ class InferenceWorker(GeneratorWorker):
                 "If the image is not from a layer, an original should always be available"
             )
 
-        if from_layer:
-            if i != 0:
-                raise ValueError(
-                    "A layer's ID should always be 0 (default value)"
-                )
+        if from_layer and i != 0:
+            raise ValueError("A layer's ID should always be 0 (default value)")
 
-            if semantic_labels and semantic_labels.shape != original.shape:
-                semantic_labels = utils.correct_rotation(semantic_labels)
-                if semantic_labels.shape != original.shape:
-                    logger.debug("WARNING: semantic labels shape mismatch")
-            if crf_results and crf_results.shape != original.shape:
-                crf_results = utils.correct_rotation(crf_results)
-                if crf_results.shape != original.shape:
-                    logger.debug("WARNING: CRF results shape mismatch")
-            if instance_labels and instance_labels.shape != original.shape:
-                instance_labels = utils.correct_rotation(instance_labels)
-                if instance_labels.shape != original.shape:
-                    logger.debug("WARNING: instance labels shape mismatch")
+        # semantic_labels = self._correct_results_rotation(semantic_labels, shape) # done at the level of model_output already
+        # instance_labels = self._correct_results_rotation(instance_labels, shape)
+        # crf_results = self._correct_results_rotation(crf_results, shape)
 
         return InferenceResult(
             image_id=i + 1,
@@ -618,12 +623,15 @@ class InferenceWorker(GeneratorWorker):
             aniso_transform=self.aniso_transform,
         )
 
-        if out.shape != inputs.shape:
-            out = utils.correct_rotation(out)
-            if out.shape != inputs.shape:
-                self.log.warning(
-                    f"Output shape {out.shape} does not match input shape {inputs.shape}, please check for extra channel/batch dimensions"
-                )
+        out = utils.correct_rotation(out)
+        extra_dims = len(inputs.shape) - 3
+        inputs_shape_corrected = np.swapaxes(
+            inputs, extra_dims, 2 + extra_dims
+        ).shape
+        if out.shape[-3:] != inputs_shape_corrected[-3:]:
+            logger.debug(
+                f"Output shape {out.shape[-3:]} does not match input shape {inputs_shape_corrected[-3:]} on HWD dims even after rotation"
+            )
         self.save_image(out, i=i)
         instance_labels, stats = self.get_instance_result(out, i=i)
         if self.config.use_crf:
@@ -703,6 +711,15 @@ class InferenceWorker(GeneratorWorker):
             post_process_transforms,
             aniso_transform=self.aniso_transform,
         )
+        out = utils.correct_rotation(out)
+        extra_dims = len(image.shape) - 3
+        layer_shape_corrected = np.swapaxes(
+            image, extra_dims, 2 + extra_dims
+        ).shape
+        if out.shape[-3:] != layer_shape_corrected[-3:]:
+            logger.debug(
+                f"Output shape {out.shape[-3:]} does not match input shape {layer_shape_corrected[-3:]} on HWD dims even after rotation"
+            )
         self.save_image(out, from_layer=True)
 
         instance_labels, stats = self.get_instance_result(

napari_cellseg3d/code_plugins/plugin_model_inference.py
@@ -556,7 +556,7 @@ class Inferer(ModelFramework, metaclass=ui.QWidgetSingleton):
         image_id = result.image_id
         model_name = self.model_choice.currentText()
 
-        viewer.dims.ndisplay = 3
+        # viewer.dims.ndisplay = 3 # let user choose
         viewer.scale_bar.visible = True
 
         if self.config.show_original and result.original is not None:
@@ -585,7 +585,11 @@ class Inferer(ModelFramework, metaclass=ui.QWidgetSingleton):
             fractions_per_channel = utils.channels_fraction_above_threshold(
                 result.result, 0.5
             )
-            index_channel_least_labelled = np.argmin(fractions_per_channel)
+            index_channel_sorted = np.argsort(fractions_per_channel)
+            for channel in index_channel_sorted:
+                if result.result[channel].sum() > 0:
+                    index_channel_least_labelled = channel
+                    break
             viewer.dims.set_point(
                 0, index_channel_least_labelled
             )  # TODO(cyril: check if this is always the right axis
@@ -609,12 +613,26 @@ class Inferer(ModelFramework, metaclass=ui.QWidgetSingleton):
             )
 
             if len(result.instance_labels.shape) >= 4:
-                max_objs_channel = np.argmin(
+                channels_by_labels = np.argsort(
                     result.instance_labels.sum(axis=(1, 2, 3))
                 )
+                min_objs_channel = channels_by_labels[0]
+                # if least labeled is empty, use next least labeled channel
+                for i in range(1, len(channels_by_labels)):
+                    if (
+                        np.unique(
+                            result.instance_labels[
+                                channels_by_labels[i]
+                            ].flatten()
+                        ).size
+                        > 1
+                    ):
+                        min_objs_channel = channels_by_labels[i]
+                        break
+
                 number_cells = (
                     np.unique(
-                        result.instance_labels[max_objs_channel].flatten()
+                        result.instance_labels[min_objs_channel].flatten()
                     ).size
                     - 1
                 )
@@ -631,9 +649,6 @@ class Inferer(ModelFramework, metaclass=ui.QWidgetSingleton):
                 result.stats, list
             ):  # list for several channels
                 logger.debug(f"len stats : {len(result.stats)}")
-                logger.debug(
-                    f"Stats dicts : {[s.get_dict() for s in result.stats if s is not None]}"
-                )
 
                 for i, stats in enumerate(result.stats):
                     # stats = result.stats
@@ -660,7 +675,7 @@ class Inferer(ModelFramework, metaclass=ui.QWidgetSingleton):
                             logger.debug(
                                 f"Length of stats array : {[len(s) for s in stats.get_dict().values()]}"
                             )
-                            logger.debug(f"Stats dict : {stats.get_dict()}")
+                            # logger.debug(f"Stats dict : {stats.get_dict()}")
 
     def _setup_worker(self):
         if self.folder_choice.isChecked():