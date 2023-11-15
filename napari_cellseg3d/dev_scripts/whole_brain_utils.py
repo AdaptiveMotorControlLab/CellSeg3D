f37ea8d30141e52711b93c752f16168a0e589d7f
C-Achard <cyril.achard@epfl.ch>
2023-11-15 11:58:59 +0100

    Update whole_brain_utils.py

--- napari_cellseg3d/dev_scripts/whole_brain_utils.py
+++ napari_cellseg3d/dev_scripts/whole_brain_utils.py
@@ -22,10 +22,14 @@ def get_boundaries(image_regions):
     return boundaries
 
 
-def remove_boundaries_from_segmentation(image_segmentation, image_labels=None):
+def remove_boundaries_from_segmentation(
+    image_segmentation, image_labels=None, image=None
+):
     """Remove boundaries from segmentation."""
     if image_labels is None:
         image_regions = extract_continuous_region(image_segmentation)
+    elif image is not None:
+        image_regions = extract_continuous_region(image)
     else:
         image_regions = image_labels
     boundaries = get_boundaries(image_regions)
