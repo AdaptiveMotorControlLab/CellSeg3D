766ceaa89e142e829dd83e32d26ba719d40504ee
C-Achard <cyril.achard@epfl.ch>
2024-12-21 12:12:15 +0100

    Temp disable channel first

--- napari_cellseg3d/code_models/worker_training.py
+++ napari_cellseg3d/code_models/worker_training.py
@@ -201,11 +201,11 @@ class WNetTrainingWorker(TrainingWorkerBase):
         patch_func = Compose(
             [
                 LoadImaged(keys=["image"], image_only=True),
-                EnsureChannelFirstd(
-                    keys=["image"],
-                    channel_dim="no_channel",
-                    strict_check=False,
-                ),
+                # EnsureChannelFirstd(
+                #     keys=["image"],
+                #     channel_dim="no_channel",
+                #     strict_check=False,
+                # ),
                 RandSpatialCropSamplesd(
                     keys=["image"],
                     roi_size=(
@@ -287,11 +287,11 @@ class WNetTrainingWorker(TrainingWorkerBase):
         load_single_images = Compose(
             [
                 LoadImaged(keys=["image"]),
-                EnsureChannelFirstd(
-                    keys=["image"],
-                    channel_dim="no_channel",
-                    strict_check=False,
-                ),
+                # EnsureChannelFirstd(
+                #     keys=["image"],
+                #     channel_dim="no_channel",
+                #     strict_check=False,
+                # ),
                 Orientationd(keys=["image"], axcodes="PLI"),
                 SpatialPadd(
                     keys=["image"],
